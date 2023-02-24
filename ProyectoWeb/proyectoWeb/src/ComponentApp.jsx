5460845575716019e2fbd12463382f07887be91a
LuisaMaria21 <88382889+LuisaMaria21@users.noreply.github.com>
2023-02-23 23:30:29 -0500

    Challenge UseFetch :D

--- ProyectoWeb/proyectoWeb/src/ComponentApp.jsx
+++ ProyectoWeb/proyectoWeb/src/ComponentApp.jsx
@@ -1,20 +1,27 @@
 import { useState } from "react"
 import { AddCategory } from "./AddCategory";
 import  GiftGrid  from "./GiftGrid";
-
 import React from 'react';
 import useCounter from './useCounter';
+import useFetch from "./UseFetch";
+
 
 function ComponentApp() {
-  const { count, increment, decrement, reset } = useCounter();
+    const { data, isLoading, hasError } = useFetch('https://api.breakingbadquotes.xyz/v1/quotes/1');
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (hasError) {
+    return <div>Error: {hasError.message}</div>;
+  }
 
   return (
     <div>
-        <h1>Challenge counter </h1>
-      <p>Count: {count}</p>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={reset}>Reset</button>
+      <h1>Challenge useFetch</h1> 
+      <p>{data[0].quote}</p>
+      <p>- {data[0].author}</p>
     </div>
   );
 }
---
+++ ProyectoWeb/proyectoWeb/src/UseFetch.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+
+function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const api = await fetch(url);
+        const data = await api.json();
+        setData(data);
+        setIsLoading(false);
+      } catch (error) {
+        setHasError(error);
+        setIsLoading(false);
+      }
+    }
+
+    fetchData();
+  }, [url]);
+
+  return { data, isLoading, hasError };
+}
+
+export default useFetch;
